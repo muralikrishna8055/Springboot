61c55aae7ca26b46251ac12f25a811dacc244c41
MuraliKrishna <mailstomuralikrishna@gmail.com>
2026-08-05 17:00:00 +0530

    get,post created

--- src/main/java/com/basic/app/Springboot/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.basic.app.Springboot.repository;
-
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class UserRepository {
-}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users values(101,"kred@gmail.com","Kred","Samuel","USER","105-ssn","Kred105");
+insert into users values(102,"kred@gmail.com","Kred","Samuel","USER","105-ssn","Kred105");
+insert into users values(103,"kred@gmail.com","Kred","Samuel","USER","105-ssn","Kred105");
