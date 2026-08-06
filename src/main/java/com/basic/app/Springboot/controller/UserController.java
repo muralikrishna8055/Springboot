54663a441e224318b7b7124a71918c096c78d61c
MuraliKrishna <mailstomuralikrishna@gmail.com>
2026-08-06 14:52:17 +0530

    custom exeception added

--- src/main/java/com/basic/app/Springboot/controller/UserController.java
+++ src/main/java/com/basic/app/Springboot/controller/UserController.java
@@ -3,9 +3,12 @@ package com.basic.app.Springboot.controller;
 import com.basic.app.Springboot.dto.UserDto;
 import com.basic.app.Springboot.dto.UserResponse;
 import com.basic.app.Springboot.entity.User;
+import com.basic.app.Springboot.exception.UserNotFoundException;
 import com.basic.app.Springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,15 +35,33 @@ public class UserController {
         return userService.createUser(user);
     }
 
+
+
     @GetMapping("/users/{id}")
     public Optional<User> getUserById(@PathVariable("id") Long id){
-        return userService.getUserById(id);
+
+        try{
+            return userService.getUserById(id);
+        }
+        catch (UserNotFoundException ex){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,ex.getMessage());
+        }
     }
+
+
+
 
     @PutMapping("/users/{id}")
     public User updateUserId(@PathVariable Long id, @RequestBody User user){
-        return userService.updateUserById(id,user);
+        try{
+            return userService.updateUserById(id,user);
+        }
+        catch (UserNotFoundException ex){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,ex.getMessage());
+        }
     }
+
+
 
     @DeleteMapping("/users/{id}")
     public String deleteUserById(@PathVariable Long id){
--- src/main/java/com/basic/app/Springboot/service/UserService.java
+++ src/main/java/com/basic/app/Springboot/service/UserService.java
@@ -3,6 +3,7 @@ package com.basic.app.Springboot.service;
 
 import com.basic.app.Springboot.dto.UserResponse;
 import com.basic.app.Springboot.entity.User;
+import com.basic.app.Springboot.exception.UserNotFoundException;
 import com.basic.app.Springboot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -51,15 +52,26 @@ public class UserService {
 
 
     //get user by id
-    public Optional<User> getUserById(Long id){
+    public Optional<User> getUserById(Long id) throws UserNotFoundException {
         //return userRepository.findById(id);
         Optional<User> user=userRepository.findById(id);
+
+        if(!user.isPresent()){
+            throw new UserNotFoundException("User not found");
+        }
+
         return user;
     }
 
 
     //update user
-    public User updateUserById(Long id,User user){
+    public User updateUserById(Long id,User user) throws UserNotFoundException{
+
+        Optional<User> optionalUser=userRepository.findById(id);
+        if(!optionalUser.isPresent()){
+            throw new UserNotFoundException("user not found in db");
+        }
+
         user.setId(id);
         return userRepository.save(user);
     }
@@ -78,7 +90,7 @@ public class UserService {
     }
 
 
-    //find user by user name
+    //find user by username
     public Optional<User> getUserByUserName(String userName){
         return userRepository.findByUsername(userName);
     }
